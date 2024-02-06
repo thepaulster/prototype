fabb2366aeaf2d716c843175b09d03853a2bff4a
Asiimwe Paul <kpasiimwe@gmail.com>
2024-02-06 21:49:43 +0300

    Player run, camera run
    added obstacle scenes and added player run and jump

--- scripts/Dino.gd
+++ scripts/Dino.gd
@@ -2,13 +2,25 @@ extends KinematicBody2D
 
 const GRAVITY : int = 2200 #force pulling player down
 const JUMP_SPEED : int = -900 #force to jump up
+const JUMP = Vector2(0, 1)
 
 var velocity : Vector2 = Vector2()
+
+func _ready():
+	$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
 	
 	velocity.y += GRAVITY * delta 
-	if Input.is_action_pressed("ui_accept"):
-		velocity.y = JUMP_SPEED
+	if is_on_floor():
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite.play("duck")
+		else:
+			$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("jump")
 	
-	move_and_slide(velocity)
+	velocity.y = move_and_slide(velocity, Vector2.UP).y
+	
---
+++ scripts/Main.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+# game variables
+const DINO_START_POS := Vector2(79, 278)
+const CAM_START_POS := Vector2(320, 180)
+
+var speed : float
+const START_SPEED: float = 5.0
+const MAX_SPEED: int = 15
+var screen_size: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport().size
+	print(screen_size)
+	new_game()
+
+
+func new_game():
+	#reset all nodes
+	$Dino.position = DINO_START_POS
+	$Dino.velocity = Vector2(0, 0)
+	$Camera2D.position = CAM_START_POS
+	$Ground.position = Vector2(0, 0)
+
+
+func _physics_process(delta):
+	speed = START_SPEED
+	
+	#move dino and camera
+	$Dino.position.x += speed
+	$Camera2D.position.x += speed
+	
